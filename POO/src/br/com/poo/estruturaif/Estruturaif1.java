1c7a25e99a96f19e4f040d382554ae5bb4683935
JotaTMBR <joao.vasena@senacsp.edu.br>
2023-03-14 09:52:57 -0300

    Estrutura if 2 - Média dos Alunos

--- POO/src/br/com/poo/estruturaif/Estruturaif1.java
+++ POO/src/br/com/poo/estruturaif/Estruturaif1.java
@@ -1,8 +1,25 @@
 package br.com.poo.estruturaif;
 
+import java.util.Scanner;
+
 public class Estruturaif1 {
 	public static void main(String args[]) {
 		System.out.println("================ Valor Par ou Impar =================");
 		System.out.println("\nEntre com um valor Numerico:");
+		// declaração da variavel
+		int numero;
+		// Permite a entrada de dados via teclado pelo terminal
+		Scanner entrada = new Scanner(System.in);
+		numero = entrada.nextInt();
+		
+		// vamos testar se o numero digitado é par ou impar
+		if(numero % 2 == 0) {
+			System.out.println("O numero: "+numero+" é Par");
+	
+		}
+		else {
+			System.out.println("O numero: "+numero+" é Impar");
+		}
+		
 	}
 }
