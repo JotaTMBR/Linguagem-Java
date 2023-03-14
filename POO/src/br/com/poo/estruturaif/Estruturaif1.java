82122196765f10108fa1de5522bf3c7eb50f8ace
JotaTMBR <joao.vasena@senacsp.edu.br>
2023-03-14 09:17:51 -0300

    Programação Orientada a Objeto e Estruturas

---
+++ POO/src/br/com/poo/estruturaif/Estruturaif1.java
@@ -0,0 +1,8 @@
+package br.com.poo.estruturaif;
+
+public class Estruturaif1 {
+	public static void main(String args[]) {
+		System.out.println("================ Valor Par ou Impar =================");
+		System.out.println("\nEntre com um valor Numerico:");
+	}
+}
